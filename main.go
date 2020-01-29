598dfa9279fab87fe9a04ba0c6d1980e27d22eea
SUHROB <SRahmonov>
2020-01-29 14:02:30 +0500

    fixed:#1

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ func main() {
 }
 func amountOfBonus(sales []int) int {
 	sumOfBonus := 0
-	minAmountBonus := 10_000
+	const minAmountBonus = 10_000
 	const bonusPercent = 5
 	const hundredPercent = 100
 	for _, sale := range sales {
